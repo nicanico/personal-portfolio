912af8d283bc2afe2e8f53e15a8bf910d8a99dc3
nicanico <nicsousan@gmail.com>
2023-02-19 14:12:23 -0300

    update 19/02 - inicio do projeto

---
+++ src/componets/header/header.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import './header.css';
+
+const Header = () => {
+    return(
+        <div className="n-wrapper">
+            <div className="n-left">
+                <div className="n-name">
+                    <span>&#123;</span>
+                    <p>nicole</p>
+                    <span>&#125;</span>
+                </div>
+                <span>toggle</span>
+            </div>
+            <div className="n-right">
+                <div className="n-list">
+                    <ul>
+                        <li><a href="#">home</a></li>
+                        <li><a href="#">sobre mim</a></li>
+                        <li><a href="#">skills e formação</a></li>
+                        <li><a href="#">projetos</a></li>
+                        <li><a href="#">contato</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Header
