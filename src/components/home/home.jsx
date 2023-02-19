9ca3814d92b71af757ff3cf765284739b208f69e
nicanico <nicsousan@gmail.com>
2023-02-19 14:54:12 -0300

    update 19/02 - ajustes

---
+++ src/components/home/home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () =>{
+    return(
+        <div className="home">
+            <div className="h-left"></div>
+            <div className="h-right"></div>
+        </div>
+    )
+}
+
+export default Home
