38f79a6da25829aba82bd66bf804a8ed8a3038a7
nicanico <nicsousan@gmail.com>
2023-02-22 18:04:26 -0300

    update 22/02 - ajutes e inicio da seção about-me

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import './App.css';
 import Header from './components/header/header';
-import Home from './components/home/home'
+import Home from './components/home/home';
 
 function App() {
   return (
